03ef43322c4b0da5b6852f8f8b2f76b61e52fd6f
Maobugichi <immansamuel846@gmail.com>
2026-09-09 11:00:21 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import ErrorPage from "./components/errorPage";
 import { Login } from "./components/auth/login";
 import { Signup } from "./components/auth/signup";
 import { RewardsHub } from "./components/rewards/rewardsHub";
-import { ProtectedRoute } from "./components/protectedRoute";
+import { ProtectedRou } from "./components/protectedRoute";
 
 
 
